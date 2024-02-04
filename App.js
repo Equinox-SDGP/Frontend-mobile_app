44c51fe9ecbee1145a5c0c9c5d8b7845d9a98580
nimeshaperi <112054998+nimeshaperi@users.noreply.github.com>
2024-02-05 00:17:03 +0530

    Tamagui configured (not completely)

--- App.js
+++ App.js
@@ -1,7 +1,30 @@
 import Home from "./src/screens/Home";
+import tamaguiConfig from "./tamagui.config";
+import { TamaguiProvider, View } from "tamagui";
+import { StyleSheet, useColorScheme } from "react-native";
+import { Button, Progress, Input } from "tamagui";
 
 export default function App() {
+  const colorScheme = useColorScheme();
   return (
-    <Home />
+    <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme}>
+      <View style={styles.container}>
+        <Home />
+        <Button onPress={() => console.log("Button pressed")}>Press me</Button>
+        <Progress value={60}>
+          <Progress.Indicator animation="bouncy" />
+        </Progress>
+        <Input size="$4" borderWidth={2} />
+      </View>
+    </TamaguiProvider>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+});
+
