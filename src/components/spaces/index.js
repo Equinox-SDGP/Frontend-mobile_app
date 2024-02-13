111c82cbdde0bcdf33324a24de6ac756d7349f86
Promodh00 <promodhmadusha@gmail.com>
2024-02-13 14:25:10 +0530

    Chat bot UI and main funtions done

--- src/components/spaces/index.js
+++ src/components/spaces/index.js
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { Button, Card, H2, Image, Paragraph, XStack} from "tamagui";
+import { Button, Card, H2, Image, Paragraph, XStack } from "tamagui";
 
 export default function SpaceCard() {
   return (
     <DemoCard
       animation="bouncy"
       size="$4"
-      width={350}
-      height={200}
+      width={320}
+      height={170}
       scale={0.9}
       hoverStyle={{ scale: 0.925 }}
       pressStyle={{ scale: 0.875 }}
@@ -21,13 +21,15 @@ export function DemoCard(props) {
       <Card.Header padded>
         <H2>Home</H2>
 
-        <Paragraph theme="alt2">Now available</Paragraph>
+        <Paragraph theme="alt2">Colombo,sri lanka</Paragraph>
       </Card.Header>
 
       <Card.Footer padded>
+        <Text>24KWh</Text>
+
         <XStack flex={1} />
 
-        <Button borderRadius="$10">Purchase</Button>
+        <Button borderRadius="$10">Healthy</Button>
       </Card.Footer>
 
       <Card.Background>
