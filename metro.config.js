cac8c7a97940d5e0b762f4a573f0fcb179c51ce2
nimeshaperi <112054998+nimeshaperi@users.noreply.github.com>
2024-02-13 21:16:56 +0530

    Imported svg and fixed spaces card

--- metro.config.js
+++ metro.config.js
@@ -2,14 +2,27 @@
 const { getDefaultConfig } = require('expo/metro-config')
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname, {
+const defaultConfig = getDefaultConfig(__dirname, {
   // [Web-only]: Enables CSS support in Metro.
   isCSSEnabled: true,
 })
 
+defaultConfig.transformer = {
+  ...defaultConfig.transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer'),
+};
+
+defaultConfig.resolver = {
+  ...defaultConfig.resolver,
+  assetExts: defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg'),
+  sourceExts: [...defaultConfig.resolver.sourceExts, 'svg'],
+};
+
+module.exports = defaultConfig;
+
 // 2. Enable Tamagui
 const { withTamagui } = require('@tamagui/metro-plugin')
-module.exports = withTamagui(config, {
+module.exports = withTamagui(defaultConfig, {
   components: ['tamagui'],
   config: './tamagui.config.ts',
   outputCSS: './tamagui-web.css',
