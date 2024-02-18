78e9271c45d5b6076ae5dee9c0c20116b603fd98
Oshada Perera <oshada.20222343@iit.ac.lk>
2024-02-19 00:44:17 +0530

    The navbar images are added, but the tab names are shown below.

--- src/app/_layout.js
+++ src/app/_layout.js
@@ -10,6 +10,7 @@ import { TamaguiProvider } from "tamagui";
 import { config } from "../../tamagui.config";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
+import { Image, Text, View , StyleSheet} from "react-native";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -50,29 +51,65 @@ function RootLayoutNav() {
   return (
     <TamaguiProvider config={config} defaultTheme={colorScheme}>
       <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <Tabs>
-          <Tabs.Screen
-            name="index"
+        <Tabs style={styles.container}>
+          <Tabs.Screen 
+            name="index" // This is the default route.
             options={{
-              title: "Home",
+              tabBarIcon:({focused}) => (
+                <View style={styles.navIcon}>
+                <Image
+                className="icon"
+                  source={require("../assets/icons/home.png")}
+                  style={{ width: 23, height: 23, tintColor: focused ? "#e32f45" : "#748c94" }}
+                />
+                <Text style={{color:focused ? "#e32f45" : "#748c94", fontSize:11}}>HOME</Text>
+                </View>
+              ),
             }}
           />
           <Tabs.Screen
             name="chatbot/index"
             options={{
-              title: "Chatbot",
+              tabBarIcon:({focused}) => (
+                <View style={styles.navIcon}>
+                <Image
+                className="icon"
+                  source={require("../assets/icons/chatbot.png")}
+                  style={{ width: 23, height: 23, tintColor: focused ? "#e32f45" : "#748c94" }}
+                />
+                <Text style={{color:focused ? "#e32f45" : "#748c94", fontSize:11}}>CHATBOT</Text>
+                </View>
+              ),
             }}
           />
           <Tabs.Screen
             name="devices/index"
             options={{
-              title: "Devices",
+              tabBarIcon:({focused}) => (
+                <View style={styles.navIcon}>
+                <Image
+                className="icon"
+                  source={require("../assets/icons/device.png")}
+                  style={{ width: 23, height: 23, tintColor: focused ? "#e32f45" : "#748c94" }}
+                />
+                <Text style={{color:focused ? "#e32f45" : "#748c94", fontSize:11}}>DEVICES</Text>
+                </View>
+              ),
             }}
           />
           <Tabs.Screen
             name="profile/index"
             options={{
-              title: "Profile",
+              tabBarIcon:({focused}) => (
+                <View style={styles.navIcon}>
+                <Image
+                className="icon"
+                  source={require("../assets/icons/profile.png")}
+                  style={{ width: 23, height: 23, tintColor: focused ? "#e32f45" : "#748c94"}}
+                />
+                <Text style={{color:focused ? "#e32f45" : "#748c94", fontSize:11}}>PROFILE</Text>
+                </View>
+              ),
             }}
           />
         </Tabs>
@@ -80,3 +117,16 @@ function RootLayoutNav() {
     </TamaguiProvider>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  navIcon: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 10,
+    paddingBottom: 5,
+  }, 
+});
