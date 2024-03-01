33355f050387ef9cb0cbe9988ce4892917ff40f7
nimeshaperi <112054998+nimeshaperi@users.noreply.github.com>
2024-03-01 23:58:03 +0530

    Fixed the graph changes with interval

---
+++ src/components/productionCard/components/graph/index.js
@@ -0,0 +1,51 @@
+import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { BarChart } from "react-native-gifted-charts";
+import ValueIndicator from "../valueIndicator";
+import graphInformation from "./graphInformation";
+import graphConfig from "./graphConfigurations";
+
+const Graph = ({ interval }) => {
+  useEffect(() => {
+    setBarData(graphInformation[interval].data);
+    setBarConfig(graphConfig[interval]);
+  }, [interval]);
+
+  const [barData, setBarData] = useState(graphInformation[interval].data);
+  const [barConfig, setBarConfig] = useState(graphConfig[interval]);
+
+  function changeActiveData(index) {
+    setBarData((prev) => {
+      return prev.map((item, i) => {
+        if (i === index) {
+          return { ...item, active: true, frontColor: "#FF621F" };
+        }
+        return { ...item, active: false, frontColor: "#E2E2E2" };
+      });
+    });
+  }
+  return (
+    <BarChart
+      style={styles.graphContainer}
+      data={barData}
+      {...graphConfig.common}
+      {...barConfig}
+      onPress={(value, index) => {
+        changeActiveData(index);
+      }}
+      renderTooltip={(item, index) => {
+        return item.active ? <ValueIndicator value={item.value} /> : null;
+      }}
+    />
+  );
+};
+
+export default Graph;
+
+const styles = StyleSheet.create({
+  graphContainer: {
+    width: "90%",
+    height: 150,
+    position: "relative",
+  },
+});
