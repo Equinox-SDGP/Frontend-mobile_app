9d4ec2a93d7181bcfb497757dab165b631b2494d
Promodh00 <promodhmadusha@gmail.com>
2024-03-09 13:31:55 +0530

    Headder panel update

--- src/components/ProfilePic/index.js
+++ src/components/ProfilePic/index.js
@@ -1,28 +1,47 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import React from "react";
-import { Avatar, XStack, YStack } from "tamagui";
+import { Avatar, XStack, YStack, Button } from "tamagui";
+import Notification from "@/assets/icons/notification.png";
 
 export default function ProfilePic() {
   return (
-    <XStack alignItems="center" space="$6">
-      <Avatar circular size="$6">
-        <Avatar.Image
-          accessibilityLabel="Cam"
-          src="https://images.unsplash.com/photo-1548142813-c348350df52b?&w=150&h=150&dpr=2&q=80"
-        />
-        <Avatar.Fallback backgroundColor="$blue10" />
-      </Avatar>
-      <YStack>
-        <Text style={styles.Greeting}> Good Morning </Text>
-        <Text style={styles.UserName}> Promodh Madusha </Text>
-      </YStack>
+    <XStack style={styles.mainContaner}>
+      <XStack gap={10}>
+        <Avatar circular size="$4">
+          <Avatar.Image
+            accessibilityLabel="Cam"
+            src="https://images.unsplash.com/photo-1548142813-c348350df52b?&w=150&h=150&dpr=2&q=80"
+          />
+          <Avatar.Fallback backgroundColor="$blue10" />
+        </Avatar>
+        <YStack>
+          <Text style={styles.Greeting}> Good Morning </Text>
+          <Text style={styles.UserName}> Promodh Madusha </Text>
+        </YStack>
+      </XStack>
+      {/* <Button alignSelf="center" icon={Airplay} size="$6"/> */}
+
+      <TouchableOpacity>
+        <Image style={styles.notification} source={Notification}></Image>
+      </TouchableOpacity>
     </XStack>
   );
 }
 
 const styles = StyleSheet.create({
   Greeting: {
-    fontSize: 16,
+    fontSize: 13,
   },
-  UserName: { fontSize: 20 },
+  UserName: { fontSize: 18 },
+  notification: {
+    width: 33,
+    height: 33,
+    marginRight: 20,
+  },
+  mainContaner: {
+    justifyContent: "space-between",
+    width: "100%",
+    alignItems: "center",
+    padding: 10,
+  },
 });
