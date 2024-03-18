3c1f5b7b2be5d6c054a5cf439332bff4079f6cde
nimeshaperi <112054998+nimeshaperi@users.noreply.github.com>
2024-03-18 18:37:56 +0530

    Changes

---
+++ src/components/onboardingItem/index.js
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import OnboardingItem from "./../onboardingItem";
+export default index = ({ data }) => {
+  return (
+    <View style={styles.container}>
+      <View style={{ flex: 3 }}>
+        <FlatList
+          data={slides}
+          renderItem={({ item }) => <OnboardingItem item={item} />}
+          horizontal
+          showsHorizontalScrollIndicator
+          pagingEnabled
+          bounces={false}
+          keyExtractor={(item) => item.id}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { x: scrollx } } }],
+            {
+              useNativeDriver: false,
+            }
+          )}
+          scrollEventThrottle={32}
+          onViewableItemsChanged={viewableItemsChanged}
+          viewabilityConfig={viewConfig}
+          ref={slidesRef}
+        />
+      </View>
+    </View>
+  );
+};
