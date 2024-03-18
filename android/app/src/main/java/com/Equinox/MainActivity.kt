a0e01248b42bfdc3d4b0e96b7a023c172acb5089
nimeshaperi <112054998+nimeshaperi@users.noreply.github.com>
2024-03-18 15:19:39 +0530

    Fixed broken icon change

--- android/app/src/main/java/com/Equinox/MainActivity.kt
+++ android/app/src/main/java/com/Equinox/MainActivity.kt
@@ -8,8 +8,6 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
-import org.devio.rn.splashscreen.SplashScreen; 
-
 import expo.modules.ReactActivityDelegateWrapper
 
 class MainActivity : ReactActivity() {
