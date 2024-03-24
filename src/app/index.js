14f2977a8a9675f4b77ae05c2725e0cc1ec987c3
Oshada Perera <oshada.20222343@iit.ac.lk>
2024-03-25 02:06:16 +0530

    notification and startpage bugs fixed

--- src/app/index.js
+++ src/app/index.js
@@ -2,23 +2,41 @@ import React, { useEffect, useState } from 'react';
 import { Redirect, Stack } from 'expo-router';
 import { useAuth0 } from 'react-native-auth0';
 import { View, ActivityIndicator } from 'react-native';
+import * as Notifications from 'expo-notifications';
 
 const Index = () => {
   const { user, error } = useAuth0();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Set loading to false once user data is fetched
-    if (user || error) {
+    // Set loading to false after 3 seconds
+    const timer = setTimeout(() => {
       setLoading(false);
-    }
-  }, [user, error]);
+    }, 2000);
+
+    // Clear the timer on component unmount
+    return () => clearTimeout(timer);
+
+    // Send notification when the component mounts
+    sendNotification();
+  }, []);
+
+  // Function to send a notification
+  const sendNotification = async () => {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'Welcome!',
+        body: 'Thank you for downloading Equinox.',
+      },
+      trigger: null, // Send immediately
+    });
+  };
 
   if (loading) {
     // Show loading indicator while waiting for user data
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-              <Stack.Screen options={{ header: () => null }} />
+        <Stack.Screen options={{ header: () => null }} />
         <ActivityIndicator size="large" color="#0000ff" />
       </View>
     );
